de1b54ac13cb9f36352e34c6991085a6067b89a4
iamdlynm <dbum.35@hotmail.com>
2018-02-08 09:40:48 -0700

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RecipesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
