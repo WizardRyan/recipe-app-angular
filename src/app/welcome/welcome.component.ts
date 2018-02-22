fd36f0c22c7632d71f4fd61426e3e8baeab74697
iamdlynm <dbum.35@hotmail.com>
2018-02-22 13:34:05 -0700

    added required fields to login section and hid password input

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {MatSnackBar} from '@angular/material';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
@@ -13,6 +14,13 @@ export class WelcomeComponent implements OnInit {
   uName: string;
   iEmail: string;
   iPassword: string;
+  uNameCtrl: FormControl = new FormControl('', [Validators.required]);
+  uEmailCtrl: FormControl = new FormControl('', [Validators.required]);
+  uPasswordCtrl: FormControl = new FormControl('', [Validators.required]);
+  iEmailCtrl: FormControl = new FormControl('', [Validators.required]);
+  iPasswordCtrl: FormControl = new FormControl ('', [Validators.required]);
+
+
 
   constructor(public auth: AuthService, public snackBar: MatSnackBar) {
   }
